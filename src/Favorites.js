13ce4e71354dd245c23ff9dc16dba10244573e7e
SenaitEkubai <Senaitekubai@gmail.com>
2021-04-04 12:00:34 +0100

    styles

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,14 +1,15 @@
 import React from "react";
-const Favorites = ({ data, fav, key }) => {
+const Favorites = ({ data, fav }) => {
   const namesList = fav.map((id) => {
-    let x = data.filter((el) => el.id === id);
+    const name = data.filter((el) => el.id === id);
 
     return (
-      <button key={id} className={x[0].sex === "f" ? "female" : "male"}>
-        {x[0].name}
+      <button key={id} className={name[0].sex === "f" ? "female" : "male"}>
+        {name[0].name}
       </button>
     );
   });
+  for (var i = 0; i < namesList.length; i++) {}
 
   return (
     <div className="favorite">
--- src/GenderNameButtons.js
+++ src/GenderNameButtons.js
@@ -2,9 +2,15 @@ import React from "react";
 const GenderNameButtons = (props) => {
   return (
     <div>
-      <button onClick={props.allGenderHandler}>All</button>
-      <button onClick={props.boyFunction}>Boy</button>
-      <button onClick={props.girlFunction}>Girl</button>
+      <button onClick={props.allGenderHandler} className="all">
+        All
+      </button>
+      <button onClick={props.boyFunction} className="boy bg-primary ml-2 mr-2">
+        Boy
+      </button>
+      <button onClick={props.girlFunction} className="girl">
+        Girl
+      </button>
     </div>
   );
 };
